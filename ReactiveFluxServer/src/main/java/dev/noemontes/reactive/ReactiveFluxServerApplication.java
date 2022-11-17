317789a283458a8889fa11a5dc1df483e7dacfb3
dark-wave <noe.montes.developer@gmail.com>
2022-11-17 13:43:26 +0100

    First commit from server side reactive project

---
+++ ReactiveFluxServer/src/main/java/dev/noemontes/reactive/ReactiveFluxServerApplication.java
@@ -0,0 +1,13 @@
+package dev.noemontes.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveFluxServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveFluxServerApplication.class, args);
+	}
+
+}
