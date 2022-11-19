f4f76f62ff5c36cc48aa96ca913187aa5fa65c46
dark-wave <noe.montes.developer@gmail.com>
2022-11-19 16:26:48 +0100

    Websocket emitter config and websocket client listen

--- ReactiveFluxServer/src/main/java/dev/noemontes/reactive/ReactiveFluxServerApplication.java
+++ ReactiveFluxServer/src/main/java/dev/noemontes/reactive/ReactiveFluxServerApplication.java
@@ -9,5 +9,4 @@ public class ReactiveFluxServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveFluxServerApplication.class, args);
 	}
-
 }
--- ReactiveWebClient/src/main/java/dev/noemontes/reactive/client/ReactiveWebClientApplication.java
+++ ReactiveWebClient/src/main/java/dev/noemontes/reactive/client/ReactiveWebClientApplication.java
@@ -1,7 +1,15 @@
 package dev.noemontes.reactive.client;
 
+import java.net.URI;
+import java.time.Duration;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ReactiveWebClientApplication {
@@ -9,7 +17,16 @@ public class ReactiveWebClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveWebClientApplication.class, args);
 		
-		UserWebClient userWebClient = new UserWebClient();
-		userWebClient.consumer();
+		WebSocketClient client = new ReactorNettyWebSocketClient();
+		
+		client.execute(
+				URI.create("ws://localhost:8080/event-emitter"), 
+				session -> session.send(
+						Mono.just(session.textMessage("event-spring-reactive-client-websocket")))
+							.thenMany(session.receive()
+									.map(WebSocketMessage::getPayloadAsText)
+									.log())
+							.then())
+				.block(Duration.ofSeconds(10L));
 	}
 }
